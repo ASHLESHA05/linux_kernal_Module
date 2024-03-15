f607515982ae14209b5fd07921313e6a9cb26d86
ashlesh <115142220+ASHLESHA05@users.noreply.github.com>
2024-03-15 13:37:15 +0530

    Create user.c

---
+++ user.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h> // for fork()
+#include <sys/wait.h>
+#include <errno.h>
+#define DEVICE "/dev/kernal" // Corrected device name
+
+int main() {
+    int fd = 0;
+    char ch, write_buf[100], read_buf[100];
+    pid_t pid;
+
+    // Fork a child process
+    pid = fork();
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) { // Parent process
+        // Print parent process ID
+        printf("Parent process: PID = %d\n", getpid());
+
+        // Open the device file
+        fd = open(DEVICE, O_RDWR);
+        if (fd == -1) {
+            perror("Failed to open the device file");
+            exit(EXIT_FAILURE);
+        }
+
+        // Prompt for user input
+        printf("\nParent process: r = read from device, w = write to device\nEnter command: ");
+        scanf(" %c", &ch);
+
+        switch (ch) {
+            case 'w':
+                printf("Enter data: ");
+                scanf(" %[^\n]", write_buf);
+                if (write(fd, write_buf, sizeof(write_buf)) == -1) {
+                    perror("Write failed");
+                }
+                break;
+            case 'r':
+                if (read(fd, read_buf, sizeof(read_buf)) == -1) {
+                    if (errno == EAGAIN) {
+                        printf("No data available in the device file\n");
+                    } else {
+                        perror("Read failed");
+                    }
+                } else {
+                    printf("Data read from device: %s\n", read_buf);
+                }
+                break;
+            default:
+                printf("Command not recognized\n");
+                break;
+        }
+
+        // Close the device file in parent
+        close(fd);
+
+        // Wait for child process to complete
+        wait(NULL);
+
+        printf("Parent process terminated\n");
+    } else { // Child process
+        // Print child process ID
+        pid_t p1=fork();
+        if(p1<0){
+            printf("fork failed for grandchild");
+        }
+        if(p1==0){
+
+            printf("In grand  grand_child id:%d\n",getpid());
+            sleep(10);
+            printf("Grand child terminating...\n");
+            exit(EXIT_SUCCESS);
+        }else{
+            printf("Child process: PID = %d\n", getpid());
+            sleep(10);
+            wait(NULL);
+
+            printf("child process terminating...\n");
+            exit(EXIT_SUCCESS);
+
+        }
+
+    }
+
+    return 0;
+}
